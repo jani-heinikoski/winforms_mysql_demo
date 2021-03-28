78e8d704739eb5bce6359b95a66199dc1306edce
jani-heinikoski <59805433+jani-heinikoski@users.noreply.github.com>
2021-03-28 14:42:45 +0300

    Customer class to represent a customer table row as an object

---
+++ MySQLDemo/Customer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySQLDemo
+{
+    class Customer
+    {
+        private long ID;
+        private String name;
+        private String address;
+        private String email;
+
+        public Customer(long ID, String name, String address, String email)
+        {
+            this.ID = ID;
+            this.name = name;
+            this.address = address;
+            this.email = email;
+        }
+
+        public long getID()
+        {
+            return ID;
+        }
+
+        public String getName()
+        {
+            return name;
+        }
+
+        public String getAddress()
+        {
+            return address;
+        }
+
+        public String getEmail()
+        {
+            return email;
+        }
+    }
+}
