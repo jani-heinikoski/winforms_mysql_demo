aa9d1403a5c781e8b3006df7450427659cbf0736
jani-heinikoski <59805433+jani-heinikoski@users.noreply.github.com>
2021-03-28 14:43:49 +0300

    First working ver

--- MySQLDemo/Form1.cs
+++ MySQLDemo/Form1.cs
@@ -12,9 +12,26 @@ namespace MySQLDemo
 {
     public partial class Form1 : Form
     {
+        private DataManager dataManager;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataManager = DataManager.GetInstance();
+            List<Customer> customers = dataManager.getCustomers();
+
+            if (customers != null)
+            {
+                Console.WriteLine($"Found {customers.Count} customers:");
+                foreach (Customer c in customers) {
+                    Console.WriteLine($"{c.getID()}|{c.getName()}");
+                }
+            }
+            
         }
     }
 }
